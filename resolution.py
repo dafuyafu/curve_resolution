87772a93cf9abb6e9d20ff405b7f560678b246cb
dafuyafu <dafuyafu@gmail.com>
2020-02-14 00:28:07 +0900

    add slide

--- resolution.py
+++ resolution.py
@@ -10,9 +10,10 @@ from blowing_up import blowing_up as bu
 if __name__ == '__main__':
 	x = symbols('x')
 	y = symbols('y')
-	# f = x ** 2 * (x - 1) - y ** 2
-	# f = x ** 3 - y ** 2
-	f = x ** 25 - y ** 19
+	f = x ** 34 - y ** 29
+	# f = x ** 3 - (x - y) ** 2 # 2,3-cusp with co-tr
+	# f = x ** 25 - y ** 19 # 19,25-cusp
+	# f = x ** 3 + x ** 2 - y ** 2 # node
 	sing = solve([f, diff(f, x), diff(f, y)])
 	print("f = " + str(f))
 	print("Sing V(f) = " + str(sing))
